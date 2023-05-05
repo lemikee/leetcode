150aa37e1acd6a80f5329bfc23d0990378fd321b
Mike Le <letumichael@gmail.com>
2023-05-05 10:41:35 -0700

    Time: 73 ms (29.70%), Space: 44.3 MB (45.75%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+class Node {
+    constructor(next, val) {
+        this.next = null;
+        this.val = null;
+    }
+}
+
+var mergeTwoLists = function(list1, list2) {
+    const dummyHead = new Node(null);
+    let current1 = list1;
+    let current2 = list2;
+    let tail = dummyHead;
+    
+    while (current1 && current2) {
+        if (current1.val < current2.val) {
+            tail.next = current1;
+            current1 = current1.next;
+        } else {
+            tail.next = current2;
+            current2 = current2.next;
+        }
+        
+        tail = tail.next;
+    }
+    
+    tail.next = current1 || current2;
+    return dummyHead.next;
+};
+
+
+// time complexity: O(min(m, n)) - where m and n are the lengths of the two input LLs, because we're only going to traverse as "far" as the lenght of the shorter lists
+// space complexity: O(m + n)
+
+
+// idea: use dummyHead pattern and create a new linked list which we will return
+//  to travser through both list1 and list2, using ptrs for each LL, we will attach the lesser value to the tail of the dummyHead LL
+//  but were not guranteed that both input LL will be of the same length, so we'll need to add the rest of the longer LL to our dummyHead LL, if it exists
+//  once we're done with our traversal and take care diff LL lengths, then we'll return dummyHead.next, which should be the head out our merged/sorted LL
+
